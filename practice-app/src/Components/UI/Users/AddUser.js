d658d5b3223cec486e2fd6bcebdd482aac55e0fc
meet_hansalia <meethansalia99@gmail.com>
2023-06-09 10:41:19 +0530

    initial commit

---
+++ practice-app/src/Components/UI/Users/AddUser.js
@@ -0,0 +1,12 @@
+
+const AddUser =(props)=>{
+    return(
+        <form>
+            <lable htmlFor="">Username</lable>
+            <input id="username" type="text"/>
+            <label htmlFor="">Age (Years) </label>
+            <input id="age" type="number"/>
+            <button></button>
+        </form>
+    )
+}
